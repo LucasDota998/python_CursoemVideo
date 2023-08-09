b11fd7e778367d9b34daf727d33ca715c6898f5b
LucasDota998 <lsdota@gmail.com>
2023-08-09 11:19:06 +0200

    ultimo exercicio

--- pythonexercicios/d077.py
+++ pythonexercicios/d077.py
@@ -1,2 +1,8 @@
 """Crie um programa que tenha uma tupla com varias palavras(não usar acentos). Ddepois disso, voce deve mostrar,
 para cada palavra, quais são as suas vogais"""
+palavras = ('aço', 'cobre','aluminio','ferro', 'ouro','platina','latao','niquel','prata')
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos tais vogais: ', end='')
+    for letra in p:
+        if letra in 'aeiou':
+            print(letra, end=' ')
